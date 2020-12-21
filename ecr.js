ade87066773d68984fbe4266568d29c2ee07cdc0
netapy <39877685+netapy@users.noreply.github.com>
2020-12-21 11:46:26 +0100

    ajouts divers

--- ecr.js
+++ ecr.js
@@ -105,7 +105,7 @@ let dictReplace = {
     "Suppr la note": "<button class='btnSuppr' contenteditable='false' onclick='supprLaNote()'> Supprimer la note. </button>",
     "[]": "<input type='checkbox'>",
     "- ": "&#8226; ",
-    ".tableau": "<table class='tableEcr'><tr><th>Lastname</th><th>Age</th></tr><tr><td>Smith</td><td>50</td></tr><tr><td>Jackson</td><td>94</td></tr></table>"
+    ".tableau": "<div style='position:relative; width:fit-content;'><table class='tableEcr'><div class='newColbtn' contenteditable='false' onclick='newCol(this)'>+</div><div class='newRowbtn' contenteditable='false' onclick='newCol(this)'>-</div><tr><th>Lastname</th><th>Age</th></tr><tr><td>Smith</td><td>50</td></tr><tr><td>Jackson</td><td>94</td></tr></table></div>"
 }
 
 document.querySelector("#activeNote").addEventListener('keyup', event => {
@@ -161,3 +161,8 @@ document.querySelector("#newNote").addEventListener('keyup', event => {
 })
 
 updateLists();
+
+
+const newCol = (elem) => {
+    console.log(elem)
+}
