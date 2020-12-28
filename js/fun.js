fb303cff0b383207a45b1768b7b018d00373e68a
netapy <39877685+netapy@users.noreply.github.com>
2020-12-28 19:07:07 +0100

    table fix
    contenteditable

--- js/fun.js
+++ js/fun.js
@@ -2,13 +2,15 @@ const newCol = (elem) => {
     let table = elem.parentElement.querySelector(".tableEcr");
     let row = table.insertRow();
     for (let i = 0; i < table.rows[0].cells.length; i++) {
-        row.insertCell(i);
+        let newCell = row.insertCell(i);
+        newCell.setAttribute("contenteditable",true)
     }
 }
 
 const newRow = (elem) => {
     let table = elem.parentElement.querySelector(".tableEcr");
     for (let i = 0; i < table.rows.length; i++) {
-        table.rows[i].insertCell(table.rows[i].cells.length);
+        let newCell = table.rows[i].insertCell(table.rows[i].cells.length);
+        newCell.setAttribute("contenteditable",true)
     }
 }
