e8ad235b16ed92208dad89cdc477a33ee9c4267d
netapy <39877685+netapy@users.noreply.github.com>
2021-01-12 22:37:28 +0100

    stalewhilerevalidate2

--- sw.js
+++ sw.js
@@ -1,1 +1,31 @@
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox-sw.js"),workbox.routing.registerRoute(({request:e})=>"navigate"===e.mode,new workbox.strategies.NetworkFirst({cacheName:"pages",plugins:[new workbox.cacheableResponse.CacheableResponsePlugin({statuses:[200]})]})),workbox.routing.registerRoute(({request:e})=>"style"===e.destination||"script"===e.destination||"worker"===e.destination,new workbox.strategies.StaleWhileRevalidate({cacheName:"assets",plugins:[new workbox.cacheableResponse.CacheableResponsePlugin({statuses:[200]})]})),workbox.routing.registerRoute(({request:e})=>"image"===e.destination,new workbox.strategies.CacheFirst({cacheName:"images",plugins:[new workbox.cacheableResponse.CacheableResponsePlugin({statuses:[200]}),new workbox.expiration.ExpirationPlugin({maxEntries:50,maxAgeSeconds:2592e3})]}));
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox-sw.js")
+
+workbox.routing.registerRoute(({
+    request: e
+}) => "navigate" === e.mode, new workbox.strategies.StaleWhileRevalidate({
+    cacheName: "pages",
+    plugins: [new workbox.cacheableResponse.CacheableResponsePlugin({
+        statuses: [200]
+    })]
+}))
+
+workbox.routing.registerRoute(({
+    request: e
+}) => "style" === e.destination || "script" === e.destination || "worker" === e.destination, new workbox.strategies.StaleWhileRevalidate({
+    cacheName: "assets",
+    plugins: [new workbox.cacheableResponse.CacheableResponsePlugin({
+        statuses: [200]
+    })]
+}))
+
+workbox.routing.registerRoute(({
+    request: e
+}) => "image" === e.destination, new workbox.strategies.CacheFirst({
+    cacheName: "images",
+    plugins: [new workbox.cacheableResponse.CacheableResponsePlugin({
+        statuses: [200]
+    }), new workbox.expiration.ExpirationPlugin({
+        maxEntries: 50,
+        maxAgeSeconds: 2592e3
+    })]
+}));
